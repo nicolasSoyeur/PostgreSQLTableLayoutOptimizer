58cf97d152ea9c4f89bb74ed7a812adb495d23fe
nicolasSoyeur <nicolas.soyeur.dev@gmail.com>
2025-02-15 12:39:50 -0500

    Commit the structure of the project

---
+++ src/main/java/ca/nsoy/dev/PostgreSQLTableLayoutOptimizer/PostgreSqlTableLayoutOptimizerApplication.java
@@ -0,0 +1,13 @@
+package ca.nsoy.dev.PostgreSQLTableLayoutOptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSqlTableLayoutOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreSqlTableLayoutOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/nsoy/dev/PostgreSQLTableLayoutOptimizer/PostgreSqlTableLayoutOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.nsoy.dev.PostgreSQLTableLayoutOptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreSqlTableLayoutOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
